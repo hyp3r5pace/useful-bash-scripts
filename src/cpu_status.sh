359e174140c4a3ba6d003ec7aa415d1c3d51a587
Soumyajit Deb <debsoumyajit100@gmail.com>
2020-08-15 23:23:30 +0530

    Add boot time display feature

--- src/cpu_status.sh
+++ src/cpu_status.sh
@@ -10,6 +10,9 @@ echo ""
 cpu_architecture=$(lscpu | head -n 1)
 echo $cpu_architecture
 
+cpu_bit=$(lscpu | grep "CPU op-mode(s)" | grep -o "[0-9]*-bit" | tail -n 1)
+echo "Register Bit size(CPU): $cpu_bit"
+
 # CPU model
 cpu_model=$(lscpu | grep "Model name:")
 echo $cpu_model
@@ -34,7 +37,16 @@ total_threads=$(( $threads_per_core * $num_cores ))
 echo "Number of Physical Threads(CPU): $total_threads"
 
 echo ""
-echo "------------------------------------------------------------------"
+echo ""
+echo "-------------------- ACTIVE INFORMATION ---------------------------"
+echo ""
+
+curr_epoch_duration=$(date +%s)
+temp=$(cat /proc/stat | grep "btime")
+boot_time_epoch_duration=$(echo $temp | awk '{print $2}')
+active_duration_seconds=$(( $curr_epoch_duration - $boot_time_epoch_duration ))
+echo "Time duration since last boot: $((${active_duration_seconds} / 3600)) hours: \
+$((${active_duration_seconds} % 3600 / 60)) minutes : $((${active_duration_seconds} % 3600 % 60)) seconds"
 
 echo ""
 echo "-----------------------------------------------------------------------------------------------------------"
