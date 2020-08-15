3529bcee7f43b966e0f45e838308ab23c1ba62cc
Soumyajit Deb <debsoumyajit100@gmail.com>
2020-08-16 02:01:00 +0530

    Add bash script for displaying number of active processes

--- src/cpu_status.sh
+++ src/cpu_status.sh
@@ -41,12 +41,31 @@ echo ""
 echo "-------------------- ACTIVE INFORMATION ---------------------------"
 echo ""
 
+# Display time duration since last boot
 curr_epoch_duration=$(date +%s)
 temp=$(cat /proc/stat | grep "btime")
 boot_time_epoch_duration=$(echo $temp | awk '{print $2}')
 active_duration_seconds=$(( $curr_epoch_duration - $boot_time_epoch_duration ))
 echo "Time duration since last boot: $((${active_duration_seconds} / 3600)) hours: \
 $((${active_duration_seconds} % 3600 / 60)) minutes : $((${active_duration_seconds} % 3600 % 60)) seconds"
+
+# Display number of active processes
+# using "ps" to display process list only display the processes which are initiated as a part/on the WSL platform. Host windows processes are never recorded.
+# Would have appreciated if both windows and WSL stats were combined and provided. Windows support is not available now!
+# But the command will work fine in a real linux kernels
+
+num_active_processes=$(ps -ef | wc -l)
+echo "Number of active processes: $(($num_active_processes - 3))"
+# Here 3 is subtracted to minus the count of ps, pipe and wc processes themselves.
+# These 3 processes are used in the first place to find the count of active processes.
+
+# Display list of active processes
+# only display the active processes which are initated in WSL platform and not the host Windows platform.
+
+
+
+# TODO: Understand /proc/loadaverage and /proc/uptime
+
 
 echo ""
 echo "-----------------------------------------------------------------------------------------------------------"
